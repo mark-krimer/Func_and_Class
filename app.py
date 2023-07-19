6ead0b4fd3e4b38f729466e6628c6c1a2fd82653
Mark Krimerman <mark.krimeran@gmail.com>
2023-07-19 16:58:33 -0400

    Python Function In HTML Working

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+
+app = Flask(__name__, template_folder="temp")
+
+def say_hello():
+    return 'Hello!'
+
+@app.route("/")
+def home():
+    return render_template("index.html", say_hello=say_hello)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
