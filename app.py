3622b2e56a8be7822952054b715296decd810390
Mark Krimerman <mark.krimeran@gmail.com>
2023-07-20 18:49:42 -0400

    Class Function Working

--- app.py
+++ app.py
@@ -2,6 +2,14 @@ from flask import Flask, render_template
 
 app = Flask(__name__, template_folder="temp")
 
+class person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def say_hello(self):
+        return f'Hello, {self.name}!'
+
 def multiply(*args):
     product = 1
     for n in args:
@@ -13,7 +21,6 @@ def review(*args):
     for n in args:
         sum = sum + n
     return sum
-
 
 def ver(age, experience):
     if age >= 10:
@@ -27,16 +34,14 @@ def ver(age, experience):
         elif experience == "no":
             return "Access to the Flask course is closed."
 
-def say_hello(name):
-    return f'Hello, {name}!'
-
 @app.route("/")
 def home():
+    mike = person("mark", 13)
     return render_template("index.html",
-                           say_hello=say_hello,
                            ver=ver,
                            review=review,
-                           multiply=multiply)
+                           multiply=multiply,
+                           user = mike)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
