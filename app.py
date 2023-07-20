5e16ffea4ca52c1cac86fec868a92e084df63615
Mark Krimerman <mark.krimeran@gmail.com>
2023-07-20 13:25:52 -0400

    Completed Task One

--- app.py
+++ app.py
@@ -2,12 +2,25 @@ from flask import Flask, render_template
 
 app = Flask(__name__, template_folder="temp")
 
+def ver(age, experience):
+    if age >= 10:
+        if experience == "yes":
+            return "Welcome to the Flask course!"
+        elif experience == "no":
+            return "Programming experience required to learn the Flask course!"
+    if age < 10:
+        if experience == "yes":
+            return "Welcome, young genius! Let's start learning Flask!"
+        elif experience == "no":
+            return "Access to the Flask course is closed."
+    
+
 def say_hello(name):
     return f'Hello, {name}!'
 
 @app.route("/")
 def home():
-    return render_template("index.html", say_hello=say_hello)
+    return render_template("index.html", say_hello=say_hello, ver=ver)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
